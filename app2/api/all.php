01aa5884d362b4cd222aed304edf9d13a4ca7053
joohnny3 <johnny31258@gmail.com>
2023-07-17 01:06:56 +0800

    async/await-read 練習

--- app2/api/all.php
+++ app2/api/all.php
@@ -1,10 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json; charset=utf-8');
-$pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=ajax_student','root','');
+// header('Content-Type: application/json; charset=utf-8');
+$pdo = new PDO('mysql:host=localhost;charset=utf8;dbname=ajax_student', 'root', '');
 //$pdo=new PDO('mysql:host=localhost;charset=utf8;dbname=s1120226','s1120226','s1120226');
 
-$students=$pdo->query("select * from students")->fetchAll();
+$students = $pdo->query("select * from students")->fetchAll();
 /* foreach($students as $student){
     echo $student['name']. '-'.$student['birthday'];
     echo "<br>";
